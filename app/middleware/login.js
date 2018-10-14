8273cf225c78545f1ef851f2d38a2ff7ff7de5a2
richole <richole@macdeMacBook-Pro-3.local>
2018-10-14 17:09:39 +0800

    更新登陆验证

--- app/middleware/login.js
+++ app/middleware/login.js
@@ -3,5 +3,5 @@ module.exports = async (ctx, next) => {
         await next()
         return
     }
-    this.ctx.service.ajax.error("账户已失效，请重新登录", 999)
+    ctx.service.ajax.error("账户已失效，请重新登录", 999)
 }
