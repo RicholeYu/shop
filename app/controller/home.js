8a7c5c9e2437a57fb99adab5a9d31d6caa28d5f6
richole <richole@macdeMacBook-Pro-3.local>
2018-10-14 17:03:52 +0800

    更新跨域

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,7 +3,10 @@ const Controller = require("egg").Controller
 
 class HomeController extends Controller {
     async common() {
-        this.ctx.body = ''
+        this.ctx.body = ' '
+        this.ctx.set("Access-Control-Allow-Origin", "*")
+        this.ctx.set("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE")
+        this.ctx.set("Access-Control-Allow-Headers", "Accept, Cache-Control, Content-Type, Language")
     }
 }
 
